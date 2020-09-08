5195a8d8b40437f9d78848dfa04d8d493bd24b53
Benjamin Sundvall <benjamin@urgott.se>
2020-09-08 14:21:52 +0200

    updated 3a and added 3b

--- uppgift_3a.py
+++ uppgift_3a.py
@@ -2,32 +2,32 @@ def new_board():
     return({})
 
 def is_free(board, column, row):
-    return(not (column, row) in board) 
+    return(not (column, row) in board)
 
 def place_piece(board, column, row, player):
-    if is_free(board, column, row):
-        board[(column, row)] = player   #fix
+    if is_free(board, column, row):             # check if the current tile is free
+        board[(column, row)] = player           # place piece
         return(True)
     else:
         return(False)
 
 def get_piece(board, column, row):
-    if not is_free(board, column, row):
-        return(board[(column, row)])
+    if not is_free(board, column, row):         # check if there is a piece on the current tile
+        return(board[(column, row)])            # return content of target tile
     else:
         return(False)
 
-def remove_piece(board, column, row):       #fix
-    if not is_free(board, column, row):
-        board.pop((column, row))
+def remove_piece(board, column, row):
+    if not is_free(board, column, row):         # check if there is a piece on the current tile
+        board.pop((column, row))                # remove tile from dictionary
         return(True)
     else:
         return(False)
 
 def move_piece(board, column, row, newColumn, newRow):
-    if not is_free(board, column, row) and is_free(board, newColumn, newRow):
-        board[(newColumn, newRow)] = board[(column, row)]
-        remove_piece(board, column, row)
+    if not is_free(board, column, row) and is_free(board, newColumn, newRow):   # check if there is a piece on the current tile and the target tile is free
+        board[(newColumn, newRow)] = board[(column, row)]                       # place new piece on target tile
+        remove_piece(board, column, row)                                        # remove old piece
         return(True)
     else:
         return(False)
@@ -40,17 +40,17 @@ def count(board, axis, axisNumber, player):     #inverterad rad/kolumn i instruk
         axisIndex = 1
 
     for piece in board:
-        if piece[axisIndex] == axisNumber and get_piece(board,piece[0], piece[1]) == player: 
+        if piece[axisIndex] == axisNumber and get_piece(board,piece[0], piece[1]) == player:    # check if the piece is on the correct row
            numberOfPieces += 1
     return(numberOfPieces)
 
 def nearest_piece(board, column, row):
-    if not board:           #check if board is empty
+    if not board:                       # check if board is empty
         return(False)
-    
-    lowestSqrDist = 999999999999
 
-    for piece in board:
+    lowestSqrDist = 10E99               # set a very high initial lowest distance
+
+    for piece in board:                 # check squared distance between piece and current tile
         dx = piece[0] - column
         dy = piece[1] - row
         sqrDist = dx*dx + dy*dy
@@ -58,4 +58,3 @@ def nearest_piece(board, column, row):
             lowestSqrDist = sqrDist
             nearestPiece = piece
     return(nearestPiece)
-
---
+++ uppgift_3b.py
@@ -0,0 +1,8 @@
+# Fast version
+def choose(n, k):
+    k = min(k, n-k)                             # optimize for large k:s
+
+    if k==0 or k==n:
+        return 1
+    else:
+        return (n // k) * choose(n-1, k-1)      # n choose k is equal to n/k * n-1 choose k-1
